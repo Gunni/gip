29e8571ca2686d8273534b3a40b1792dec0cba38
Gunnar Guðvarðarson <gunni@wave.is>
2017-01-02 06:44:11 +0000

    Make code testable

--- gip.py
+++ gip.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 import sys, argparse, ipaddress as IP
+from typing import Any, List
+
+
+class TooManyException(ValueError):
+	def __init__(self):
+		super(TooManyException, self).__init__('> 2^8 (256) addresses, refusing to print, force with --force\n')
+
+
+class CannotParseIPException(ValueError):
+	def __init__(self, message):
+		super(CannotParseIPException, self).__init__(message)
 
 
 def decodeBits(net: [IP.IPv4Interface, IP.IPv6Interface]) -> str:
@@ -12,67 +23,86 @@ def decodeBits(net: [IP.IPv4Interface, IP.IPv6Interface]) -> str:
 	return 'Global'
 
 
-def printKeyVal(key: str, value: object, pre: str = '') -> None:
-	print('{pre}{key: <{padding}} - {value}'.format(
+def formatKeyVal(key: str, value: object, pre: str = '') -> str:
+	return '{pre}{key: <{padding}} - {value}\n'.format(
 		pre = pre,
 		padding = 15,
 		key = str(key),
 		value = str(value))
-	)
 
 
-def displayNetwork(net: [IP.IPv4Interface, IP.IPv6Interface]) -> None:
-	v = {'version': net.version, 'type': decodeBits(net)}
-	print('[IPv{version} Network] {type} address'.format(**v))
-	printKeyVal('IP address', net.ip)
-	printKeyVal('Network address', net.network)
+def format_network(net: [IP.IPv4Interface, IP.IPv6Interface]) -> str:
+	res = ''
+
+	res += '[IPv{version} Network] {type} address\n'.format(version = net.version, type = decodeBits(net))
+	res += formatKeyVal('IP address', net.ip)
+	res += formatKeyVal('Network address', net.network)
 
 	if net.version == 4:  # never used for IPv6
-		printKeyVal('Subnet mask'.format(**v), net.netmask, pre = '\n')
-		printKeyVal('Wildcard mask'.format(**v), net.hostmask)
+		res += formatKeyVal('Subnet mask', net.netmask, pre = '\n')
+		res += formatKeyVal('Wildcard mask', net.hostmask)
 
 	if net.network.num_addresses > 2:
-		printKeyVal('Network range', (net.network.network_address).exploded, pre = '\n')
-		printKeyVal('', (net.network.broadcast_address).exploded)
+		res += formatKeyVal('Network range', (net.network.network_address).exploded, pre = '\n')
+		res += formatKeyVal('', (net.network.broadcast_address).exploded)
 	else:
-		printKeyVal('Network range', 'Literally one address', pre = '\n')
+		res += formatKeyVal('Network range', 'Literally one address', pre = '\n')
 
 	if net.network.num_addresses > 2:
-		printKeyVal('Usable range', (net.network.network_address + 1).exploded, pre = '\n')
-		printKeyVal('', (net.network.broadcast_address - 1).exploded)
+		res += formatKeyVal('Usable range', (net.network.network_address + 1).exploded, pre = '\n')
+		res += formatKeyVal('', (net.network.broadcast_address - 1).exploded)
 	else:
-		printKeyVal('Usable range', 'Same as Network range due to network size', pre = '\n')
+		res += formatKeyVal('Usable range', 'Same as Network range due to network size', pre = '\n')
+
+	return res
 
 
-def listAllIPs(net: [IP.IPv4Interface, IP.IPv6Interface], force: bool) -> None:
+def list_ips(net: [IP.IPv4Interface, IP.IPv6Interface], force: bool) -> [IP.IPv4Network, IP.IPv6Network]:
 	if net.network.num_addresses > 2 ** 8 and force == False:
-		sys.stderr.write('> 2^8 (256) addresses, refusing to print, force with --force')
-		sys.exit(2)
+		raise TooManyException()
 
-	for ip in net.network:
-		print(ip)
+	return net.network
 
 
-if __name__ == '__main__':
+def parse_args(args: List[str]) -> List[Any]:
+	del args[0]
+
 	try:
 		parser = argparse.ArgumentParser(
 			description = 'Parse an IP address of any version and display useful information about it.')
 		parser.add_argument('ip', nargs = '*', help = 'An IPv4 or IPv6 address or subnet')
 		parser.add_argument('--list', '-l', action = 'store_true', help = 'Just list all the IPs in the subnet')
 		parser.add_argument('--force', action = 'store_true', help = 'Force list all ips for large subnets')
-		args = parser.parse_args()
+		args = parser.parse_args(args)
 	except SystemExit:  # just to make sure it exits uncleanly on -h
 		sys.exit(3)
 
+	return args
+
+
+def parse_ip(arg: str) -> [IP.IPv4Interface, IP.IPv6Interface]:
+	try:
+		return IP.ip_interface(arg)
+	except ValueError as e:
+		raise CannotParseIPException(str(e)) from e
+
+
+if __name__ == '__main__':
+	args = parse_args(sys.argv)
 	arg = '/'.join(args.ip)
 
 	try:
-		ipaddr = IP.ip_interface(arg)
-	except ValueError as e:
+		subnet = parse_ip(arg)
+	except CannotParseIPException as e:
 		sys.stderr.write(str(e))
 		sys.exit(1)
 
 	if args.list:
-		listAllIPs(ipaddr, args.force)
-	else:
-		displayNetwork(ipaddr)
+		try:
+			print(list_ips(subnet, args.force))
+			sys.exit(0)
+		except TooManyException as e:
+			sys.stderr.write(str(e))
+			sys.exit(2)
+
+	print(format_network(subnet))
