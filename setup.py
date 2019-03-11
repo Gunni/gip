cd561cff445c7b22570a16086d1aa42b9b6d9230
Gunnar Guðvarðarson <gunnar.gudvardarson@advania.is>
2019-03-11 18:31:31 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='gip',
+	version='1.0.0',
+	packages=['gip'],
+        author='Gunnar Guðvarðarson',
+        author_email='gunnar@meh.is',
+        url='https://git.system.is/meh/gip',
+)
+
